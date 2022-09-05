0636324554dd0f60c301ba8eecabffb21ee07a7b
alexander-drabik <xelada91@o2.pl>
2022-09-05 20:39:02 +0200

    Add multiple requests

--- src/main/java/xyz/goatcode/GoatCodeController.java
+++ src/main/java/xyz/goatcode/GoatCodeController.java
@@ -1,12 +1,20 @@
 package xyz.goatcode;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GoatCodeController {
     @RequestMapping("/")
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("subpage", "index");
+        return "goatcode";
+    }
+
+    @RequestMapping("/kontakt")
+    public String kontakt(Model model) {
+        model.addAttribute("subpage", "kontakt");
         return "goatcode";
     }
 }
