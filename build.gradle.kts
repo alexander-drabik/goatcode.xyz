f9ed8c462a013b165927c23ef909699b66514139
alexander-drabik <xelada91@o2.pl>
2022-09-06 21:40:45 +0200

    Check for subscriber count on social blade

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.jsoup:jsoup:1.15.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
--- src/main/java/xyz/goatcode/Application.java
+++ src/main/java/xyz/goatcode/Application.java
@@ -1,11 +1,34 @@
 package xyz.goatcode;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        try {
+            Document doc = Jsoup.connect("https://socialblade.com/youtube/channel/UCEl6qkGmXm1OpmqjYPvuzeQ").userAgent("Mozilla").get();
+
+            Elements elements = doc.getElementsByClass("YouTubeUserTopInfo");
+
+            for(Element element : elements) {
+                if (element.text().length() >= 11) {
+                    if (element.text().substring(0, 11).equals("Subscribers")) {
+                        String[] splited = element.text().substring(12).split("\\s+");
+                        System.out.println(splited[0]);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //SpringApplication.run(Application.class, args);
     }
 }
